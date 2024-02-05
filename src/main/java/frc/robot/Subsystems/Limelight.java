9e4f5fc4040872aeeb9cd36970cdeb23b216be4c
Ryan040709 <ryanma040709@gmail.com>
2024-02-05 18:41:59 -0500

    fix merge error

--- src/main/java/frc/robot/Subsystems/Limelight.java
+++ src/main/java/frc/robot/Subsystems/Limelight.java
@@ -2,108 +2,123 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Subsystems;
+package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.CommandSwerveDrivetrain;
 
-public final class Limelight {
-  public final static int Left = 0;
-  public final static int Right = 1;
-  public final Pose3d LimelightPose = new Pose3d(2.23, 18.16, 15, new Rotation3d(0, 0, 0)).times(.0254);
+/** Add your docs here. */
+public class Limelight extends SubsystemBase{
+   //calling other subsystems
+    private final LimelightHelpers limeLight = new LimelightHelpers();
+ XboxController controller1 = new XboxController(0);
+ 
 
-  public final static NetworkTable limeLightLeft = NetworkTableInstance.getDefault().getTable("limelight-left");
-  public final static NetworkTable limeLightRight = NetworkTableInstance.getDefault().getTable("limelight-right");
+ public double leftXAxis;
+ public double leftYAxis;
+ public double RightXAxis;
 
-  public static int getClosest() {
-    if (ta(Left) <= ta(Right)) {
-      return Left;
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+   static double positionY = 3;
+   static double positionX = -2;
+    static double rotationPositon = -180;
+    double forwardtpm = 0;
+    double lefttpm = 0;
+    double rotationtmp = 0;
+    public double leftaxis = 0;
+    // settign pid values
+    PIDController pidX = new PIDController(.3, 0, 0);
+    PIDController pidY = new PIDController(.3, 0, 0);
+    PIDController pidRotaion = new PIDController(.003, 0, 0);
+
+  
+       
+    @Override
+    public void periodic() {
+      // setting the controller1s to a variable
+      double controllerLeftY = controller1.getLeftY();
+      double controllerLeftX = controller1.getLeftX();
+      double controllerRightX = controller1.getRightX();
+
+   
+        
+        
+        //getting the robots postion
+        double[] botpose1 = LimelightHelpers.getBotPose("");
+        double tx = botpose1[0];
+        double ty = botpose1[1];
+        double rz = botpose1[5];
+    
+
+        //setting the output of the robot based on the x and y to go to a setpoint
+        forwardtpm = pidX.calculate(tx, positionX);
+        lefttpm = pidY.calculate(ty, positionY);
+
+        rotationtmp = pidRotaion.calculate(rz, rotationPositon);
+      // setting the x-axis of the robot
+        if ((controllerLeftX>.15 ) || (controllerLeftX <-.15)){
+         leftXAxis = controllerLeftX;
+         }
+         else if(controller1.getXButton()){
+        leftXAxis = lefttpm;
+         }
+         else{
+            leftXAxis = 0;
+         }
+
+      // setting the left y-axis of the robot 
+         if ((controllerLeftY>.15) || (controllerLeftY<-.15)) { 
+           leftYAxis = controllerLeftY;
+         }else if(controller1.getXButton()){
+         leftYAxis =  forwardtpm;
+         }
+         else{
+            leftYAxis = 0;
+         }
+
+         // setting the rotation axis of the robot
+         if(controllerRightX>.15 ||(controllerRightX<-.15)){
+       RightXAxis = controllerRightX;
+         }
+         else if(controller1.getXButton()){
+            if (rotationPositon <rz){
+         RightXAxis = -rotationtmp;
+            }
+            else{
+               RightXAxis = rotationtmp;
+            }
+         }
+         else{
+            RightXAxis = 0 ;
+         }
+      //displaying the robot x and y
+        SmartDashboard.putNumber("robotsXPosition", tx);
+        SmartDashboard.putNumber("robotsYPosition", ty);
+        SmartDashboard.putNumber("robotsRotaionPosition",rz);
+       // putting the robots axis on to the dashboard
+        SmartDashboard.putNumber("leftXaxis", leftXAxis);
+        SmartDashboard.putNumber("leftYaxis", leftYAxis);
+        SmartDashboard.putNumber("rightXaxis", RightXAxis);
+        //displaying the outputs of the pid
+        SmartDashboard.putNumber("forwardPower", forwardtpm);
+        SmartDashboard.putNumber("leftPower", lefttpm);
+        SmartDashboard.putNumber("rotaionPower", rotationtmp);
+       //displaying controler controller1 posistion 
+        SmartDashboard.putNumber("controlerleftY",controllerLeftY);
+        SmartDashboard.putNumber("controlerleftX",controllerLeftX);
+        SmartDashboard.putNumber("controlerRightX",controllerRightX);
+        SmartDashboard.putBoolean("xbutton",controller1.getXButton());
+        
+        
     }
-    return Right;
-  }
-
-  /**
-   * @return if any limelight has a valid target
-   */
-  public static Boolean tv() {
-    return 
-      NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("tv").getDouble(0) == 1 ||
-      NetworkTableInstance.getDefault().getTable("limelight-right").getEntry("tv").getDouble(0) == 1;
-  }
-
-  /**
-   * 
-   * @param side which limelight, Limelight.Left or Limelight.Right, to use.
-   * @return if that specific limelight has a valid target
-   */
-  public static Boolean tv(int side) {
-    return side == Left ? 
-      limeLightLeft.getEntry("tv").getDouble(0) == 1:
-      limeLightRight.getEntry("tv").getDouble(0) == 1;
-      
-  }
-
-  /**
-   * @param side which limelight, Limelight.Left or Limelight.Right, to use.
-   * @return area of the target 0-100% 
-   */
-  public static double ta(int side) {
-    return (side == 1) ? 
-    limeLightRight.getEntry("ta").getDouble(0) : 
-    limeLightLeft.getEntry("ta").getDouble(0);
-  }
-
-  /**
-   * @param side which limelight, Limelight.Left or Limelight.Right, to use.
-   * @return the horizontal angle to the target
-   */
-  public static double tx(int side) {
-    return (side == 1) ? 
-    limeLightRight.getEntry("tx").getDouble(0) : 
-    limeLightLeft.getEntry("tx").getDouble(0);
-  }
-
-  /**
-   * @param side which limelight, Limelight.Left or Limelight.Right, to use.
-   * @return the vertical angle to the target
-   */
-  public static double ty(int side) {
-    return (side == 1) ? 
-    limeLightRight.getEntry("ty").getDouble(0) : 
-    limeLightLeft.getEntry("ty").getDouble(0);
-  }
-
-  /**
-   * @param side which limelight, Limelight.Left or Limelight.Right, to use.
-   * @return the vertical angle to the target
-   */
-  public static Pose3d botPose(int side) {
-    double[] left = {0,0,0,0,0,0};
-    double[] right = {0,0,0,0,0,0};
-    if (limeLightLeft.getEntry("botpose").getDoubleArray(new double[] {0,0,0,0,0,0}).length > 1)
-      {left = limeLightLeft.getEntry("botpose").getDoubleArray(new double[] {0,0,0,0,0,0});};
-    if (limeLightRight.getEntry("botpose").getDoubleArray(new double[] {0,0,0,0,0,0}).length > 1)
-      {right = limeLightRight.getEntry("botpose").getDoubleArray(new double[] {0,0,0,0,0,0});};
-      return (side == 1) ? 
-      new Pose3d(
-        right[0],
-        right[1],
-        right[2],
-        new Rotation3d(
-          Math.toRadians(right[3]),
-          Math.toRadians(right[4]),
-          Math.toRadians(right[5])
-      )): 
-      new Pose3d(
-        left[0],
-        left[1],
-        left[2],
-        new Rotation3d(
-          Math.toRadians(left[3]),
-          Math.toRadians(left[4]),
-          Math.toRadians(left[5])
-      ));
-  }
 }
